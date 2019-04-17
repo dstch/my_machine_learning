355813b4731d91bfa293aa0462c5acbc28694109
dstch <dstch@163.com>
2019-04-17 17:49:09 +0800

    add GAN demo file

---
+++ GAN/GAN_Demo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: dstch
+@license: (C) Copyright 2013-2019, Regulus Tech.
+@contact: dstch@163.com
+@file: GAN_Demo.py
+@time: 2019/4/17 17:45
+@desc: https://zhuanlan.zhihu.com/p/43047326
+        a simple GAN demo
+"""
