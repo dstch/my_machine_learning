a5518a836320e0524d09fdaa3abbbc1d1d15b815
dstch <dstch@163.com>
2019-06-14 19:19:23 +0800

    增加一点代码

--- NLP-Beginner/Text_Classify_by_logistic.py
+++ NLP-Beginner/Text_Classify_by_logistic.py
@@ -1,10 +1,25 @@
 import numpy as np
 import pandas as pd
-
+from sklearn.feature_extraction.text import CountVectorizer
 
 def bag_of_word(words):
-    # 将文本数据转换为词袋模型向量
-    word_list=[]
+'''
+@description: 将文本数据转换为词袋模型向量
+@param {type} 
+@return: 
+'''
+    word_list = [x.split() for x in words]
+    word_list=set(word_list)
+    for line in words:
         
 
-data=pd.read_csv('data/train.tsv')
+def vocabulary_of_word(words):
+'''
+@description: 将文本数据转换为词汇表模型向量
+@param {type} 
+@return: 
+'''    
+
+
+data = pd.read_csv('data/train.tsv',usecols=[3])
+data_x_list=np.array(data).tolist()
